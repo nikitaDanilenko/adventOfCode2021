a5bcd671dbd1ac8a31aaee0ddb65df556e94dd4f
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-12-04 00:47:11 +0100

    Add solution for day 3, puzzle 2

--- src/main/scala/day03/Day03.scala
+++ src/main/scala/day03/Day03.scala
@@ -13,14 +13,23 @@ object Day03 {
     .toList
 
   def columnsVia(compare: (Int, Int) => Boolean)(numbers: Seq[String]): Natural =
-     numbers.map(_.toList)
-      .transpose
-      .reverse
+     binaryToNatural(
+       numbers.map(_.toList)
+        .transpose
+        .zipWithIndex
+        .map { case (column, i) =>
+           val ones = column.count(_ == '1')
+           if compare(ones, column.length - ones) then '1' else '0'
+        }
+     )
+
+  def binaryToNatural(bins: Seq[Char]): Natural =
+    bins.reverse
       .zipWithIndex
-      .map { case (column, i) =>
-        val ones = column.count(_ == '1')
-        if compare(ones, column.length - ones) then Natural.one << i else Natural.zero
-      }.foldLeft(Natural.zero)(_ + _)
+      .map { case (c, i) =>
+        if c == '1' then Natural.one << i else Natural.zero
+      }
+      .foldLeft(Natural.zero)(_ + _)
 
   def gammaOf(numbers: Seq[String]): Natural =
     columnsVia(_ >= _)(numbers)
@@ -28,10 +37,44 @@ object Day03 {
   def epsilonOf(numbers: Seq[String]): Natural =
     columnsVia(_ <= _)(numbers)
 
+  def comparedBitAt(moreOnes: Char, lessOnes: Char, position: Int, numbers: Seq[String]): Char =
+    rearrange(numbers)
+      .collectFirst { case (column, i) if i == position =>
+        val ones = column.count(_ == '1')
+        if ones >= column.length - ones then moreOnes else lessOnes
+      }.getOrElse('0')
+
+  def ratingWith(lessOnes: Char, moreOnes: Char, numbers: Seq[String]): Natural =
+    val columns = numbers.headOption.fold(0)(_.length)
+    0.until(columns).foldLeft(numbers) {
+      (remainders, position) =>
+        val relevantBit = comparedBitAt(
+          moreOnes = moreOnes,
+          lessOnes = lessOnes,
+          position = position,
+          numbers = remainders
+        )
+        //unsafe, but sufficient
+        val filtered = remainders.filter(_.charAt(position) == relevantBit)
+        if filtered.nonEmpty then filtered else remainders
+    }
+      .headOption
+      .fold(Natural.zero)(s => binaryToNatural(s.toList))
+
+  private def rearrange(numbers: Seq[String]): Seq[(Seq[Char], Int)] =
+    numbers.map(_.toList)
+      .transpose
+      .zipWithIndex
+
   @main
   def solution1: Unit =
     val gamma = gammaOf(input1)
     val epsilon = epsilonOf(input1)
     pprint.log(gamma * epsilon)
 
+  @main
+  def solution2: Unit =
+    val oxygen = ratingWith('0', '1', input1)
+    val co2 = ratingWith('1', '0', input1)
+    pprint.log(oxygen * co2)
 }
