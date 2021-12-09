c46fd8d79a6bec246135d15debeba36f840a293e
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-12-09 21:34:48 +0100

    Add solution for puzzle 2, day 9

--- src/main/scala/day09/Day09.scala
+++ src/main/scala/day09/Day09.scala
@@ -32,6 +32,9 @@ object Day09 {
   def neighboursOf(position: Position, positionMap: PositionMap): List[Int] =
     neighbours.map(add(position, _)).flatMap(positionMap.get)
 
+  def neighbourPositionsOf(position: Position, positionMap: PositionMap): List[Position] =
+    neighbours.map(add(position, _)).filter(positionMap.isDefinedAt)
+
   def isLocalMinimum(position: Position, positionMap: PositionMap): Boolean =
     val mv = positionMap.get(position)
     val ns = neighboursOf(position, positionMap)
@@ -39,9 +42,33 @@ object Day09 {
       ns.forall(_ > v)
     }
 
+  def flow(lowPoint: Position): Set[Position] =
+    def repeat(visited: Set[Position], current: Set[Position]): List[Set[Position]] =
+      if current.isEmpty then List.empty
+      else
+        val increasing = current.flatMap { c =>
+          val valueOfC = input(c)
+          val newNeighbours = neighbourPositionsOf(c, input).toSet -- visited
+          newNeighbours.filter {
+            n =>
+              val valueOfN = input(n)
+              valueOfN < 9 && valueOfN > valueOfC
+          }
+        }
+        current +: repeat(visited ++ current, increasing)
+
+    repeat(Set.empty, Set(lowPoint)).foldLeft(Set.empty[Position])(_ | _)
+
   @main
   def solution1: Unit =
     val localMinPositions = input.keysIterator.filter(isLocalMinimum(_, input)).toList
     val localMinSum = localMinPositions.map(p => 1 + input(p)).sum
     pprint.log(localMinSum)
+
+  @main
+  def solution2: Unit =
+    val localMinPositions = input.keysIterator.filter(isLocalMinimum(_, input)).toList
+    pprint.log(flow((29, 39)))
+    val basinSizes = localMinPositions.map(flow(_).size).sorted.reverse.take(3).product
+    pprint.log(basinSizes)
 }
