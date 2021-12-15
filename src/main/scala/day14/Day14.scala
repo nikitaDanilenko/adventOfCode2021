8bade04d4b81e10fd791d0fa93fa435c0a9d2b8d
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-12-15 01:04:50 +0100

    Add solution for day 14, puzzle 2

--- src/main/scala/day14/Day14.scala
+++ src/main/scala/day14/Day14.scala
@@ -1,26 +1,55 @@
 package day14
 
+import spire.implicits._
+import spire.compat._
+import Translation.CharKey
+import Translation.FrequencyMap
+
 object Day14 {
 
-  val baseString: String = "OOVSKSPKPPPNNFFBCNOV"
-
-  def replace(dictionary: Map[String, String])(input: String): String =
-    input.zip(input.tail ++ " ").flatMap{ case (x, y) =>
-      val key = List(x, y).mkString
-      (x +: dictionary.getOrElse(key, "")).trim
-    }.mkString
-
-
-  def evaluate(input: String): BigInt =
-    val ordered = input.groupBy(identity)
+  val frequencyMap: FrequencyMap = {
+    val input = "OOVSKSPKPPPNNFFBCNOV"
+    input.zip(input.tail)
+      .groupBy(identity)
       .view
-      .mapValues(_.length)
+      .mapValues(ps => BigInt(ps.length))
+      .toMap
+  }
+
+  def replace(dictionary: Map[CharKey, Char])(frequencyMap: FrequencyMap): FrequencyMap =
+    def keyToKeys(key: CharKey): (CharKey, CharKey) =
+      val dictionaryValue = dictionary(key)
+      ((key._1, dictionaryValue), (dictionaryValue, key._2))
+
+    def increase(inc: BigInt)(value: Option[BigInt]): Option[BigInt] =
+      Some(value.getOrElse(BigInt(0)) + inc)
+
+    frequencyMap.toList.foldLeft(frequencyMap) { case (map, (charKey, n)) =>
+      val (key1, key2) = keyToKeys(charKey)
+      map.updatedWith(charKey)(_.map(_ - n).filter(_ > BigInt(0)))
+        .updatedWith(key1)(increase(n))
+        .updatedWith(key2)(increase(n))
+    }
+
+  def evaluate(input: FrequencyMap): BigInt =
+    val ordered = input
+      .toList
+      .flatMap { case ((c1, c2), n) => List(c1 -> n, c2 -> n) }
+      .groupBy(_._1)
+      .view
+      .mapValues(_.map(_._2).sum)
       .toList
       .sortBy(_._2)
-    ordered.last._2 - ordered.head._2
+
+    (ordered.last._2 - ordered.head._2 + BigInt(1)) / 2
 
   @main
   def solution1: Unit =
-    val stepped = List.iterate(baseString, 11)(replace(Translation.dictionary)).last
+    val stepped = List.iterate(frequencyMap, 11)(replace(Translation.dictionary)).last
+    pprint.log(evaluate(stepped))
+
+  @main
+  def solution2: Unit =
+    val stepped = List.iterate(frequencyMap, 41)(replace(Translation.dictionary)).last
     pprint.log(evaluate(stepped))
 }
