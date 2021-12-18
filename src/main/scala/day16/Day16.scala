d2ad717106e98a7485b6a20c4c601599f7d8935f
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2021-12-18 12:35:18 +0100

    Add solution for day 16, puzzle 2

--- src/main/scala/day16/Day16.scala
+++ src/main/scala/day16/Day16.scala
@@ -2,7 +2,9 @@ package day16
 
 import spire.math.Natural
 import spire.syntax.additiveMonoid._
+import spire.syntax.multiplicativeMonoid._
 import spire.implicits._
+import spire.compat._
 
 import scala.io.Source
 
@@ -21,6 +23,37 @@ object Day16 {
         collection.version + collection.packets.map(sumVersions).qsum
     }
 
+  def valueOf(packet: Packet): Natural =
+    packet match {
+      case literal: Packet.Literal =>
+        literal.number
+      case collection: Packet.Collection =>
+        val values = collection.packets.map(valueOf)
+        collection.id.toInt match {
+          case 0 =>
+            values.qsum
+          case 1 =>
+            values.qproduct
+          case 2 =>
+            values.min
+          case 3 =>
+            values.max
+          case 5 =>
+            values match {
+              case p1 :: p2 :: _ => if p1 > p2 then Natural.one else Natural.zero
+            }
+          case 6 =>
+            values match {
+              case p1 :: p2 :: _ => if p1 < p2 then Natural.one else Natural.zero
+            }
+          case 7 =>
+            values match {
+              case p1 :: p2 :: _ => if p1 == p2 then Natural.one else Natural.zero
+            }
+        }
+
+    }
+
   @main
   def solution1: Unit = {
     val result = Packet.packetParser.parse(input).map {
@@ -30,5 +63,14 @@ object Day16 {
 
     pprint.log(result)
   }
+
+  @main
+  def solution2: Unit =
+    val result = Packet.packetParser.parse(input).map {
+      case (rem, p) =>
+        valueOf(p).toString
+    }
+
+    pprint.log(result)
 
 }
