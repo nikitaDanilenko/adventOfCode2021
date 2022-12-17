bea82270ca1a5f1964e04bff0c83543b265fe4bf
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2022-12-17 01:15:03 +0100

    Add solution for day 21.2

--- src/main/scala/day21/Day21.scala
+++ src/main/scala/day21/Day21.scala
@@ -1,9 +1,11 @@
 package day21
 
-import monocle.syntax.all._
+import cats.data.State
+import monocle.syntax.all.*
 import spire.math.Natural
-import spire.syntax.additiveMonoid._
-import spire.implicits._
+import spire.syntax.additiveMonoid.*
+import spire.implicits.*
+import cats.syntax.traverse._
 
 object Day21 {
 
@@ -16,18 +18,31 @@ object Day21 {
 
   }
 
+  case class PlayerScore(
+      position: Int,
+      score: BigInt
+  )
+
   case class PlayerScores(
-    p1: BigInt,
-    pos1: Int,
-    p2: BigInt,
-    pos2: Int
+      player1: PlayerScore,
+      player2: PlayerScore
   )
 
   def nextPos(max: Int, pos: Int, value: Int): Int =
-    def step(pos: Int): Int =
-      if pos < max then 1 + pos else 1
+    (pos + value - 1) % max + 1
 
-    List.iterate(pos, 1 + value)(step).last
+  def newScoreWith(
+      modifier: Int => Int,
+      first: Boolean,
+      playerScores: PlayerScores
+  ): PlayerScores =
+    if first
+    then
+      val updated = playerScores.focus(_.player1.position).modify(modifier)
+      updated.focus(_.player1.score).modify(s => s + updated.player1.position)
+    else
+      val updated = playerScores.focus(_.player2.position).modify(modifier)
+      updated.focus(_.player2.score).modify(s => s + updated.player2.position)
 
   def move(
       first: Boolean,
@@ -38,14 +53,7 @@ object Day21 {
     val values = nextDice.map(_.value).sum
     val modifier: Int => Int = v => nextPos(10, v, values)
 
-    val newScore =
-      if first
-      then
-        val updated = playerScores.focus(_.pos1).modify(modifier)
-        updated.focus(_.p1).modify(s => s + updated.pos1)
-      else
-        val updated = playerScores.focus(_.pos2).modify(modifier)
-        updated.focus(_.p2).modify(s => s + updated.pos2)
+    val newScore = newScoreWith(modifier, first, playerScores)
 
     (!first, newScore, nextDice.last)
 
@@ -55,23 +63,123 @@ object Day21 {
   )
 
   def iterateUntil(targetScore: Int, playerScores: PlayerScores, deterministicDie: DeterministicDie): Result =
-    val xs = LazyList.iterate((true, playerScores, deterministicDie))(move.tupled)
-      .takeWhile{
-        case (f, ps, dd) => ps.p1 < targetScore && ps.p2 < targetScore
+    val xs = LazyList
+      .iterate((true, playerScores, deterministicDie))(move.tupled)
+      .takeWhile { case (f, ps, dd) =>
+        ps.player1.score < targetScore && ps.player2.score < targetScore
       }
 
     val iterations = xs.length
     val last = xs.last
-    val losing = last._2.p1.min(last._2.p2)
+    val losing = last._2.player1.score.min(last._2.player2.score)
     Result(3 * iterations, losing)
+
+  // Largely based upon https://todd.ginsberg.com/post/advent-of-code/2021/day21/
+  def quantum: (StateMap, Wins) = {
+    val frequencyMap =
+      List(
+        3 -> BigInt(1),
+        4 -> BigInt(3),
+        5 -> BigInt(6),
+        6 -> BigInt(7),
+        7 -> BigInt(6),
+        8 -> BigInt(3),
+        9 -> BigInt(1)
+      )
+
+    case class PlayerScoresWithTurn(
+        playerScores: PlayerScores,
+        first: Boolean
+    )
+
+    object PlayerScoresWithTurn {
+      def scoreReached(score: Int, playerScoresWithTurn: PlayerScoresWithTurn): Boolean =
+        playerScoresWithTurn.playerScores.player1.score >= score ||
+          playerScoresWithTurn.playerScores.player2.score >= score
+
+    }
+
+    case class Wins(
+        player1: BigInt,
+        player2: BigInt
+    )
+
+    type StateMap = Map[PlayerScoresWithTurn, Wins]
+
+    def iterate(scores: PlayerScoresWithTurn): State[StateMap, Wins] =
+      if (PlayerScoresWithTurn.scoreReached(21, scores))
+        if (scores.playerScores.player1.score > scores.playerScores.player2.score) State.pure(Wins(1, 0))
+        else State.pure(Wins(0, 1))
+      else {
+        for {
+          stateMap <- State.get[StateMap]
+          result <- stateMap
+            .get(scores)
+            .fold {
+              for {
+                newWins <- frequencyMap
+                  .traverse { (diceValue, frequency) =>
+                    val newScores = newScoreWith(v => nextPos(10, v, diceValue), scores.first, scores.playerScores)
+                    iterate(PlayerScoresWithTurn(newScores, !scores.first)).map { wins =>
+                      Wins(
+                        wins.player1 * frequency,
+                        wins.player2 * frequency
+                      )
+                    }
+                  }
+                  .map {
+                    _.reduce { (wins1, wins2) =>
+                      Wins(
+                        wins1.player1 + wins2.player1,
+                        wins1.player2 + wins2.player2
+                      )
+                    }
+                  }
+                _ <- State.modify[StateMap](_.updated(scores, newWins))
+              } yield newWins
+            }(State.pure)
+        } yield result
+
+      }
+
+    iterate(
+      PlayerScoresWithTurn(
+        PlayerScores(
+          PlayerScore(
+            score = 0,
+            position = 10
+          ),
+          PlayerScore(
+            score = 0,
+            position = 6
+          )
+        ),
+        true
+      )
+    ).run(Map.empty).value
+  }
 
   @main
   def solution1: Result =
-    val startScores = PlayerScores(p1 = 0, pos1 = 10, p2 = 0, pos2 = 6)
+    val startScores = PlayerScores(
+      PlayerScore(
+        score = 0,
+        position = 10
+      ),
+      PlayerScore(
+        score = 0,
+        position = 6
+      )
+    )
     val die = DeterministicDie(value = 100, size = 100)
     val result = iterateUntil(1000, startScores, die)
     pprint.log(result)
     pprint.log(result.iterations * result.losingScore)
     result
+
+  @main
+  def solution2(): Unit =
+    val wins = quantum()
+    pprint.log(wins._2)
 
 }
