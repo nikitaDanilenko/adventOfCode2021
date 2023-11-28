08dbf6c0a4262ac23a7dee3b36141830821fdb95
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-11-28 20:54:07 +0100

    Make reference lazy to account for missing resources

--- src/main/scala/day05/Day05.scala
+++ src/main/scala/day05/Day05.scala
@@ -11,11 +11,15 @@ import scala.io.Source
 case class Position(x: Int, y: Int)
 
 object Position {
+
   implicit val positionAdditiveMonoid: AdditiveMonoid[Position] = new AdditiveMonoid[Position] {
     override val zero: Position = Position(0, 0)
+
     override def plus(p1: Position, p2: Position): Position =
       Position(p1.x + p2.x, p1.y + p2.y)
+
   }
+
 }
 
 case class Line(from: Position, to: Position)
@@ -39,7 +43,7 @@ val lineParser: Parser[Line] =
 def parseLine(text: String): Option[Line] =
   lineParser.parse(text).toOption.map(_._2)
 
-private val input: List[Line] = Source
+private lazy val input: List[Line] = Source
   .fromResource("Day05.txt")
   .getLines()
   .flatMap(parseLine)
@@ -48,7 +52,8 @@ private val input: List[Line] = Source
 def horizontalPositions(line: Line): Seq[Position] =
   if line.from.y == line.to.y
   then
-    spire.math.min(line.from.x, line.to.x)
+    spire.math
+      .min(line.from.x, line.to.x)
       .to(spire.math.max(line.from.x, line.to.x))
       .map(Position(_, line.from.y))
   else Seq.empty
@@ -56,7 +61,8 @@ def horizontalPositions(line: Line): Seq[Position] =
 def verticalPositions(line: Line): Seq[Position] =
   if line.from.x == line.to.x
   then
-    spire.math.min(line.from.y, line.to.y)
+    spire.math
+      .min(line.from.y, line.to.y)
       .to(spire.math.max(line.from.y, line.to.y))
       .map(Position(line.from.x, _))
   else Seq.empty
@@ -70,18 +76,17 @@ def markPositions(area: Area, positions: Seq[Position]): Area =
 
 def diagonalUpPositions(line: Line): Seq[Position] =
   if line.from.x + line.from.y == line.to.x + line.to.y
-    then
-      val (first, second) = if line.from.x <= line.to.x then (line.from, line.to) else (line.to, line.from)
-      0.to(second.x - first.x).map(i => first.focus(_.x).modify(_ + i).focus(_.y).modify(_ - i))
+  then
+    val (first, second) = if line.from.x <= line.to.x then (line.from, line.to) else (line.to, line.from)
+    0.to(second.x - first.x).map(i => first.focus(_.x).modify(_ + i).focus(_.y).modify(_ - i))
   else Seq.empty
 
 def diagonalDownPositions(line: Line): Seq[Position] =
   if line.from.y - line.from.x == line.to.y - line.to.x
-    then
-      val (first, second) = if line.from.x <= line.to.x then (line.from, line.to) else (line.to, line.from)
-      0.to(second.x - first.x).map(i => first.focus(_.x).modify(_ + i).focus(_.y).modify(_ + i))
-  else
-    Seq.empty
+  then
+    val (first, second) = if line.from.x <= line.to.x then (line.from, line.to) else (line.to, line.from)
+    0.to(second.x - first.x).map(i => first.focus(_.x).modify(_ + i).focus(_.y).modify(_ + i))
+  else Seq.empty
 
 @main
 def solution1: Unit =
@@ -91,6 +96,8 @@ def solution1: Unit =
 
 @main
 def solution2: Unit =
-  val allPositions = input.flatMap(l => horizontalPositions(l) ++ verticalPositions(l) ++ diagonalUpPositions(l) ++ diagonalDownPositions(l))
+  val allPositions = input.flatMap(l =>
+    horizontalPositions(l) ++ verticalPositions(l) ++ diagonalUpPositions(l) ++ diagonalDownPositions(l)
+  )
   val markedArea = markPositions(Map.empty, allPositions)
   pprint.log(markedArea.count(_._2 > Natural.one))
