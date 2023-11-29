c53e6516d9e5837c2e848892bdbbdd8a476374fb
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-11-29 22:53:18 +0100

    Remove code, add explanation for the manual solution

--- src/main/scala/day24/Day24.scala
+++ src/main/scala/day24/Day24.scala
@@ -1,144 +1,32 @@
-package day24
+/*
+Computed entirely by hand.
 
-import cats.parse.Parser
+The operations repeat in blocks very similar instructions, only the lines 5, 6, and 11 vary.
+Also, division is either by 1, so no division, or by 26.
+If a division by 26 is not performed, there is no chance of the register z becoming 0,
+because every step contains a multiplication by 26.
 
-import scala.io.Source
-sealed trait Register
+First, simplify the operations.
+Let <!=> : (Int, Int) => Int, a <!=> b = if a != b then 1 else 0.
+Also, let l_i, m_i, n_i be the variables from the lines 5, 6, and 11, respectively.
+Finally, let d_1, ..., d_14 denote the digits of the input number.
 
-object Register {
-  case object W extends Register
-  case object X extends Register
-  case object Y extends Register
-  case object Z extends Register
+Then, in each block of operations one has the following steps
 
-  val parser: Parser[Register] = Parser.oneOf(
-    List(
-      Parser.char('w').as(W),
-      Parser.char('x').as(X),
-      Parser.char('y').as(Y),
-      Parser.char('z').as(Z)
-    )
-  )
+x = (z_(i-1) % 26 + m_i) <!=> d_i
 
-}
+z_i = z_(i-1) / 26 * (25 * x + 1) + (d_i + n_i) * x
 
-val intParser = day05.nonNegativeIntParser.orElse(
-  Parser.char('-') *> day05.nonNegativeIntParser.map(-_)
-)
+Note that when x = 0, n_i is irrelevant.
 
-sealed trait Operand
+Now, we can manually compute 14 iterations, where in each case where l_i = 26,
+we need to have x = 0, which gives us the equality: d_i = z_(i-1) % 26 + m_i.
+Incidentally, each such equality reduces the number of free variables by 1, so in the end we have at most 7 free variables.
 
-object Operand {
-  case class RegisterReference(register: Register) extends Operand
-  case class Value(int: Int) extends Operand
+I omit the actual equations, because they give insight into the input.
+However, the final set of equations contains at most seven variables that are defined in terms of the other seven.
+In my case, one such equation was d_i = d_j -8 for some i, j, which has precisely one solution, thus reducing the search space even further.
 
-  val parser: Parser[Operand] =
-    Register.parser
-      .map(RegisterReference.apply)
-      .backtrack
-      .orElse(
-        intParser.map(Value.apply)
-      )
-
-}
-
-sealed trait Instruction
-
-object Instruction {
-  case class Inp(register: Register) extends Instruction
-  case class Add(register: Register, operand: Operand) extends Instruction
-  case class Mul(register: Register, operand: Operand) extends Instruction
-  case class Div(register: Register, operand: Operand) extends Instruction
-  case class Mod(register: Register, operand: Operand) extends Instruction
-  case class Eql(register: Register, operand: Operand) extends Instruction
-
-  val spaces = Parser.char(' ').rep
-
-  val registerWithOperandParser: Parser[(Register, Operand)] =
-    for {
-      register <- Register.parser
-      _ <- spaces
-      operand <- Operand.parser
-    } yield (register, operand)
-
-  val parser: Parser[Instruction] = Parser.oneOf(
-    List(
-      (Parser.string("inp") *> spaces *> Register.parser).map(Inp.apply),
-      (Parser.string("add") *> spaces *> registerWithOperandParser).map(Add.apply),
-      (Parser.string("mul") *> spaces *> registerWithOperandParser).map(Mul.apply),
-      (Parser.string("div") *> spaces *> registerWithOperandParser).map(Div.apply),
-      (Parser.string("mod") *> spaces *> registerWithOperandParser).map(Mod.apply),
-      (Parser.string("eql") *> spaces *> registerWithOperandParser).map(Eql.apply)
-    )
-  )
-
-}
-
-lazy val input = Source
-  .fromResource("day24.txt")
-  .getLines()
-  .flatMap(Instruction.parser.parseAll(_).toOption)
-  .toList
-
-def valueOf(operand: Operand, registers: Map[Register, Int]): Int =
-  operand match
-    case Operand.RegisterReference(register) => registers(register)
-    case Operand.Value(int)                  => int
-
-case class State(
-    registers: Map[Register, Int],
-    numbers: List[Int]
-)
-
-object State {
-
-  val initialRegisters: Map[Register, Int] = Map(
-    Register.W -> 0,
-    Register.X -> 0,
-    Register.Y -> 0,
-    Register.Z -> 0
-  )
-
-}
-
-def interpret(
-    instruction: Instruction,
-    state: State
-): State =
-  val updatedNumbers = instruction match
-    case Instruction.Inp(_) => state.numbers.tail
-    case _                  => state.numbers
-
-  val updatedRegisters =
-    instruction match
-      case Instruction.Inp(register) =>
-        state.registers.updated(register, state.numbers.head)
-      case Instruction.Add(register, operand) =>
-        val value = valueOf(operand, state.registers)
-        state.registers.updated(register, state.registers(register) + value)
-      case Instruction.Mul(register, operand) =>
-        val value = valueOf(operand, state.registers)
-        state.registers.updated(register, state.registers(register) * value)
-      case Instruction.Div(register, operand) =>
-        val value = valueOf(operand, state.registers)
-        state.registers.updated(register, state.registers(register) / value)
-      case Instruction.Mod(register, operand) =>
-        val value = valueOf(operand, state.registers)
-        state.registers.updated(register, state.registers(register) % value)
-      case Instruction.Eql(register, operand) =>
-        val value = valueOf(operand, state.registers)
-        state.registers.updated(register, if state.registers(register) == value then 1 else 0)
-
-  State(
-    updatedRegisters,
-    updatedNumbers
-  )
-
-def interpretAll(instructions: List[Instruction], numbers: List[Int]): State =
-  instructions.foldLeft(State(State.initialRegisters, numbers)) { (state, instruction) =>
-    interpret(instruction, state)
-  }
-
-@main
-def solution1: Unit =
-  println(input)
+Finally, going through the equations, one can maximize or minimize each variable as far as possible,
+which yields the desired results.
+ */
